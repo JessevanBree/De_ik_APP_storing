08bbabb9ea7eab7b456b147aa61927b549af7da9
Jesse van Bree <jessevanbree@live.nl>
2018-05-31 14:31:11 +0000

    wokrin

---
+++ app/Providers/QuestionsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace De_ik_app_storing\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuestionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
